333536a1b541a5c466c99c079c430e4ee375a6a3
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2021-08-19 13:45:50 -0400

    Online Test App

--- Phase 2/Phase 2 End Project Online Test Application/online-exam-app/src/app/mdf-exam/mdf-exam.component.ts
+++ Phase 2/Phase 2 End Project Online Test Application/online-exam-app/src/app/mdf-exam/mdf-exam.component.ts
@@ -27,7 +27,15 @@ export class MdfExamComponent implements OnInit {
   }
 
   allQuestion=[
-    {question:"3 + 4", ans1:"10",ans2:"12",ans3:"19",ans4:"7",correctAns:"7"},
-    {question:"5-4",ans1:"0",ans2:"1",ans3:"9",ans4:"8",correctAns:"1"  }
-   ]
+    { question: "8+8", ans1: "16", ans2: "15", ans3: "17", ans4: "19", correctAns: "16" },
+    { question: "9+9", ans1: "18", ans2: "17", ans3: "19", ans4: "11", correctAns: "18" },
+    { question: "1+9", ans1: "18", ans2: "10", ans3: "19", ans4: "11", correctAns: "10" },
+    { question: "2+9", ans1: "18", ans2: "11", ans3: "19", ans4: "11", correctAns: "11" },
+    { question: "3+9", ans1: "12", ans2: "17", ans3: "19", ans4: "11", correctAns: "12" },
+    { question: "4+9", ans1: "18", ans2: "17", ans3: "13", ans4: "11", correctAns: "13" },
+    { question: "5+9", ans1: "18", ans2: "17", ans3: "19", ans4: "14", correctAns: "14" },
+    { question: "6+9", ans1: "18", ans2: "17", ans3: "19", ans4: "15", correctAns: "15" },
+    { question: "7+9", ans1: "18", ans2: "16", ans3: "19", ans4: "11", correctAns: "16" },
+    { question: "8+9", ans1: "18", ans2: "17", ans3: "17", ans4: "11", correctAns: "17" }
+  ]
 }
