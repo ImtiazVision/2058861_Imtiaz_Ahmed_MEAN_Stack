bb7cd658d94f8822e079e0245e340ff45aedeada
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2021-09-02 17:43:33 -0400

    socket.io chat

--- Phase 3/3. Section End Project 3 Chatting With Socket IO/server.js
+++ Phase 3/3. Section End Project 3 Chatting With Socket IO/server.js
@@ -3,10 +3,13 @@ let express = require('express');
 // The following code will create a reference to the express module
 let app = express();
 
+let http = require('http').Server(app);
+
 // The following code will load express-ws module and create the reference 
 // of express-ws with the help of express module reference using the IIFE
+let io = require('socket.io')(http);
 
-let ws = require('express-ws')(app);
+// let ws = require('express-ws')(app);
 
 // http://localhost:9090
 // The following code will open the html static web page 
@@ -14,19 +17,27 @@ app.get("/", (request, response) => {
   response.sendFile(__dirname + "//index.html");
 })
 
-app.ws("/",(socket,request)=>{ 
-  console.log("Client Connected");
+// app.ws("/",(socket,request)=>{ 
+  io.on("connection",(socket)=>{
+    console.log("Client Connected");
+    socket.on("obj",(msg)=>{
+      console.log(msg);
+      // response.send(msg);
+
+    })
+  
+    socket.emit("obj1", "Hello Client, you are connected to the server...")
+    socket.emit(Date());
+  
 
   // receive message from client application 
-  socket.on("message", (msg) => {
-    console.log("The Client says: " +msg);
+  // socket.on("message", (msg) => {
+  //   console.log("The Client says: " +msg);
     // print out the date and time when the message is received
-    socket.send(Date());
   })
 
-  socket.send("Server says : You are successfully connected to the server!!!")
-})
+  // socket.send("Server says : You are successfully connected to the server!!!")
 
 
-app.listen(9090, () => { console.log("Server is running on port 9090"); });
+http.listen(9090, () => { console.log("Server is running on port 9090"); });
 
