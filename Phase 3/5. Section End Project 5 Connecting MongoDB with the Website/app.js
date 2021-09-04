a3212c7f21ca1bda1ae172ed186ac64745ca493b
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2021-09-04 14:35:39 -0400

    done

--- Phase 3/5. Section End Project 5 Connecting MongoDB with the Website/app.js
+++ Phase 3/5. Section End Project 5 Connecting MongoDB with the Website/app.js
@@ -4,7 +4,7 @@ let express = require('express');
 // loading mongoose module
 let mongoose = require('mongoose');
 
-// to avoid lower case collection creation and adding s.
+// to avoid lower case collection creation and adding s postfix to collection name
 mongoose.pluralize(null);
 
 // referencing the express module
