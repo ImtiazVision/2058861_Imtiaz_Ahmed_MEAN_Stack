a47ac468e048006686344b8349e31af25c85da33
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2021-09-08 11:17:05 -0400

    update

--- Phase 3/5. Section End Project 5 Connecting MongoDB with the Website/app.js
+++ Phase 3/5. Section End Project 5 Connecting MongoDB with the Website/app.js
@@ -33,64 +33,33 @@ mongodb.connect(url).
 then(res=>console.log("Connected")).
 catch(err=>console.log(err));
 
-
-mongoose.connect(url).then(()=>{console.log("Connected to Mongoose");
+// connecting the mongoose database
+mongoose.connect(url).
+then(()=>{console.log("Connected to Mongoose");
 })
 
-app.get("/add",(request,response)=> {
-    response.sendFile(__dirname + "/template/addCourse.html");
+// path to the templates folder
+app.get("/template/index", (request, response) => {
+  response.sendFile('index.html', { root: __dirname });
+})
+app.get("/template/addCourse", (request, response) => {
+  response.sendFile('addCourse.html', { root: __dirname });
 })
 
-app.get("/update", (request, response) => {
-  response.sendFile(__dirname + "/template/updateCourse.html");
+app.get("/template/updateCourse", (request, response) => {
+  response.sendFile('updateCourse.html', { root: __dirname });
 })
 
-app.get("/delete", (request, response) => {
-  response.sendFile(__dirname + "/template/deleteCourse.html");
+app.get("/template/deleteCourse", (request, response) => {
+  response.sendFile('deleteCourse.html', { root: __dirname });
 })
 
-app.get("/fetch", (request, response) => {
-  response.sendFile(__dirname + "/template/fetchCourse.html");
+app.get("/template/fetchCourse", (request, response) => {
+  response.sendFile('fetchCourse.html', { root: __dirname });
 })
 
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/template/index.html");
-})
 
-io.on('connection',(socket)=>{
-  console.log('Client connected');
 
-  socket.emit('startup','Hello, you are connected.');
-})
-
-socket.on('course_added',(newCourse)=>{
-  let addedCourse = new courseCollection({_id:newCourse[0],name:newCourse[1],description:newCourse[2],course_price:newCourse[3]});
-  courseCollection.create(addedCourse,(err,result)=>{
-    if(!err) {
-      socket.emit('added_response','New course added');
-    }else {
-      socket.emit('added_response','Error in adding course');
-    }
-  })
-})
-
-socket.on('course_removed',(deleted_course)=>{
-  courseCollection.deleteOne({_id:deleted_course},(err,result)=>{
-    if(result.deletedCount>0){
-      socket.emit('removed_response','Course removed');
-    }else {
-      socket.emit('removed_response','Course not found');
-    }
-  })
-})
-
-socket.on('requested_courses',()=>{
-  courseCollection.find({},(err,doc)=>{
-    if(err){ socket.emit('Unsuccessful', 'Error in finding the intended course.')} else{
-      socket.emit('fetched_courses',doc);
-    }
-  })
-})
 
 
 http.listen(3000,()=>console.log("Server is running on port number 3000."));
